bc940d36d3791920abcc3dd7bfc39d04831505e8
Alekshamon <aleksandrahamon@gmail.com>
2024-01-12 15:30:21 +0100

    v3.7

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,9 @@ export default defineConfig({
       external: ["@geoapify/react-geocoder-autocomplete"],
     },
   },
+  build: {
+    rollupOptions: {
+      external: ["./card/Card"],
+    },
+  },
 });
