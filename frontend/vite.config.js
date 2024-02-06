46842e3b9da594cd56717ab44759fbf8e760948f
Alekshamon <aleksandrahamon@gmail.com>
2024-02-06 01:03:23 +0100

    bug deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import {
+  GeoapifyGeocoderAutocomplete,
+  GeoapifyContext,
+} from "@geoapify/react-geocoder-autocomplete";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), GeoapifyGeocoderAutocomplete(), GeoapifyContext()],
   build: {
+    rollupOptions: {
+      external: {[
+        "react", // ignore react stuff
+        "react-dom",
+      ],
+    },
+  },
+  optimizeDeps: {
+    include: ["@geoapify/react-geocoder-autocomplete"],
+  },
     outDir: "build",
   },
 });
